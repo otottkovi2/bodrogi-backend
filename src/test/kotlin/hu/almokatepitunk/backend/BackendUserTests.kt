94bfe71d04787cc2a92ef4acc7329d878498766f
szt2otokov0 <szt2otokov@vasvari.org>
2025-01-20 18:09:17 +0100

    redun user tests with MockMvc

--- src/test/kotlin/hu/almokatepitunk/backend/BackendUserTests.kt
+++ src/test/kotlin/hu/almokatepitunk/backend/BackendUserTests.kt
@@ -3,24 +3,42 @@ package hu.almokatepitunk.backend
 import hu.almokatepitunk.backend.users.UserDto
 import hu.almokatepitunk.backend.users.User
 import hu.almokatepitunk.backend.users.UserRepository
-import hu.almokatepitunk.backend.utils.SecureHttpClient
-import hu.almokatepitunk.backend.utils.SecureHttpClient.Companion.getForEntitySecure
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.json.JacksonTester
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.exchange
 import org.springframework.boot.test.web.client.getForEntity
-import org.springframework.boot.test.web.client.postForEntity
 import org.springframework.data.domain.Example
+import org.springframework.data.domain.ExampleMatcher
 import org.springframework.http.*
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
 import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.web.servlet.assertj.MockMvcTester
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.web.context.WebApplicationContext
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureJsonTesters
 class BackendUserTests {
+
+    @Autowired
+    private lateinit var applicationContext: WebApplicationContext
+
+    private lateinit var mockMvc: MockMvcTester
+
+    @Autowired
+    private lateinit var userJsonSerializer: JacksonTester<UserDto>
+
+    @Autowired
+    private lateinit var userListJsonSerializer:JacksonTester<List<UserDto>>
 
     @Autowired
     private lateinit var testRestTemplate: TestRestTemplate
@@ -28,19 +46,18 @@ class BackendUserTests {
     @Autowired
     lateinit var userRepository: UserRepository
 
-    @Autowired
-    lateinit var applicationContext: WebApplicationContext
-
-    lateinit var httpClient: SecureHttpClient
-
     private val testUser = User(
         "new user",
         "{bcrypt}\$2a\$10\$57FYrXITZfEeeIQ/7oGshuEkSQsMwiAziHINixZwfPZJH8658jPWS", "")
 
     @BeforeEach
     fun setup() {
-        if (!this::httpClient.isInitialized) httpClient = SecureHttpClient(applicationContext,"new user")
-        if(userRepository.exists(Example.of(testUser))) return
+        mockMvc = MockMvcTester.from(applicationContext) { config ->
+            config.apply<DefaultMockMvcBuilder>(springSecurity())
+                .build()
+        }
+        val exampleMatcher = ExampleMatcher.matching().withIgnorePaths("id","passwordHash")
+        if(userRepository.exists(Example.of(testUser, exampleMatcher))) return
         userRepository.save(testUser)
     }
 
@@ -49,44 +66,63 @@ class BackendUserTests {
     fun registerNewUser(){
         userRepository.deleteAll()
         val userToCreate = UserDto(testUser.username,"password")
-        val response = testRestTemplate.postForEntity<Void>("/api/register",userToCreate)
-        assertThat(response.statusCode).isEqualTo(HttpStatus.CREATED)
-        val location = response.headers.location
-        assertThat(location).isNotNull()
+        val userJson = userJsonSerializer.write(userToCreate)
+        val result = mockMvc.post()
+            .uri("/api/register")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(userJson.json)
+            .exchange()
+        assertThat(result.response.status).isEqualTo(HttpStatus.CREATED.value())
+        assertThat(result.response.getHeader("Location")).matches("http://.*/api/user/.+")
     }
 
     @Test
     @DirtiesContext
     fun dontRegisterExistingUser(){
         val userToCreate = UserDto(testUser.username,"pasword")
-        val response = testRestTemplate.postForEntity<Void>("/api/register",userToCreate)
-        assertThat(response.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
+        val userJson = userJsonSerializer.write(userToCreate)
+        val result = mockMvc.post()
+            .uri("/api/register")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(userJson.json)
+            .exchange()
+        assertThat(result.response.status).isEqualTo(HttpStatus.BAD_REQUEST.value())
     }
 
     @Test
+    @WithMockUser()
     fun getAllUsers(){
-        val debugResponse = testRestTemplate.getForEntitySecure(httpClient,"/api/users",String::class.java)
-        //todo:find a way to output a lsit type
-        val response = testRestTemplate.getForEntitySecure(httpClient,"api/users",String::class.java)
-        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
-        val userCount = response.body?.size
-        assertThat(userCount).isGreaterThan(0)
+        val result = mockMvc.get()
+            .uri("/api/user")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+        assertThat(result.response.status).isEqualTo(HttpStatus.OK.value())
+        val users = userListJsonSerializer.parse(result.response.contentAsString).`object`
+        assertThat(users).isNotNull
+        assertThat(users.size).isGreaterThan(0)
     }
 
     @Test
+    @WithMockUser()
     fun getUserById(){
-        val response = testRestTemplate.getForEntity<UserDto>("/api/user/${testUser.username}")
-        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
-        val dto = response.body
-        assertThat(dto).isNotNull()
-        val user = userRepository.findByUsername(dto!!.username)
-        assertThat(user).isEqualTo(testUser)
+        val result = mockMvc.get()
+            .uri("/api/user/{username}",testUser.username)
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+        assertThat(result.response.status).isEqualTo(HttpStatus.OK.value())
+        val user = userJsonSerializer.parse(result.response.contentAsString).`object`
+        assertThat(user).isNotNull
+        assertThat(user.username).isEqualTo(testUser.username)
     }
 
     @Test
+    @WithMockUser()
     fun dontGetNonExistingUser(){
-        val response = testRestTemplate.getForEntity<UserDto>("/api/user/fak")
-        assertThat(response.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
+        val result = mockMvc.get()
+            .uri("/api/user/{username}",testUser.username.plus(" impostor"))
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+        assertThat(result.response.status).isEqualTo(HttpStatus.NOT_FOUND.value())
     }
 
     @Test
